23c0a338184b3f21194e364e0c049f4f342ca6e9
Adameddy26 <158989293+Adameddy26@users.noreply.github.com>
2024-06-13 19:09:38 +0530

    Update fulfillment.spec.js

--- tests/fulfillment.spec.js
+++ tests/fulfillment.spec.js
@@ -231,7 +231,7 @@ test('Fulfillment', async () => {
     console.log(`${dropdownItems.length} facilities found`);
 
     let facilityRunCount = 0;
-    for (let i = 10; i < dropdownItems.length && facilityRunCount < 3; i++) {
+    for (let i = 0; i < dropdownItems.length && facilityRunCount < 3; i++) {
       await dropdownItems[i].scrollIntoViewIfNeeded();
       const facilityName = await dropdownItems[i].textContent();
       await dropdownItems[i].click();
